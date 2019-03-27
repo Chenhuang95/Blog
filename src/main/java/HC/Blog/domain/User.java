32f77ae392460fdf8eba456a3cdeb68ce83fb2a8
HC <329806042@qq.com>
2019-03-27 11:22:59 +0800

    Thymeleaf action successful

--- src/main/java/HC/Blog/domain/User.java
+++ src/main/java/HC/Blog/domain/User.java
@@ -17,40 +17,42 @@ package HC.Blog.domain;
  */
 public class User {
 
-    private long id; // 用户的唯一标识
-    private String name;
-    private int age;
-
-    public User() {
-    }
-
-    public User(long id, String name, int age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
+	private Long id;
+	private String name;
+	private String email;
+	//构造函数
+	public User()//无参构造
+	{
+		
+	}
+	
+	public User(Long id,String name,String email)
+	{
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+	
+	
+	//
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
+
+
